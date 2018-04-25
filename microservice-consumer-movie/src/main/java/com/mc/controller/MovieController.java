6efabe1f700a224e3835067c5bf0b20128537091
chuchenglong <martinchor@foxmail.com>
2018-04-25 18:16:17 +0800

    完成eureka

---
+++ microservice-consumer-movie/src/main/java/com/mc/controller/MovieController.java
@@ -0,0 +1,29 @@
+package com.mc.controller;
+
+import com.mc.model.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author ChenglongChu
+ * @description
+ * @create 2018/04/25 17:07
+ */
+@RestController
+public class MovieController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/user/{id}")
+    public UserInfo findById(@PathVariable int id) throws Exception {
+        restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        UserInfo userInfo = restTemplate.getForObject("http://chuchenglong:8001/" + id, UserInfo.class);
+        return userInfo;
+    }
+}
--- provider-user/src/main/java/com/mc/mapper/UserInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.mc.mapper;
-
-import com.mc.config.ConfigQkMapper;
-import com.mc.model.UserInfo;
-
-public interface UserInfoMapper extends ConfigQkMapper<UserInfo> {
-}
